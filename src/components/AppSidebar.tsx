b6841f722642997de72e648b707cd4d79a76c455
Max Max <enjleez1999@gmail.com>
2025-05-19 17:38:41 +0000

    قم بتصغيرها قليلا

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -27,7 +27,7 @@ import { cn } from "@/lib/utils";
 
 // Miniature App Logo for Sidebar
 const MiniAppLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-8 w-8 text-primary">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6 text-primary"> {/* Reduced size to h-6 w-6 */}
     <path d="M12 2L2 7l10 5 10-5-10-5z"/>
     <path d="M2 17l10 5 10-5"/>
     <path d="M2 12l10 5 10-5"/>
